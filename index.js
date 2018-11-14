7d4a0692e57207a6d7a962c76aee79696576ffd6
Adib <adib.pournazari@solirius.com>
2018-11-14 10:30:39 +0000

    deleting useless files

--- index.js
+++
@@ -1,7 +0,0 @@
-import { Game } from './game.js';
-import { MEDIUM } from './gameSpeed.js'
-import { Speed } from './gameSpeed.js'
-
-var game = new Game();
-game.runAtSpeed(MEDIUM);
-//game.runAtSpeed(Speed.MEDIUM);
